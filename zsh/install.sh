a73317b8530fbe81a65bf0d312cc5e3431955d01
Tyron Gabriel <51530686+tyrongabriel@users.noreply.github.com>
2024-02-06 22:45:41 +0100

    Updated zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -10,8 +10,18 @@ sudo command -v zsh | sudo teee -a /etc/shells
 # Set as default shell
 chsh -s /usr/bin/zsh
 
+# Install FiraCode font
+sudo apt install fonts-firacode
+
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install theme
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+# Install Plugins (Autosuggest & Highlighting)
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 # Create link for config
 ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
